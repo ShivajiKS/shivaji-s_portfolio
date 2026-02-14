e9b4c18e1f6a3be2df29e27f1fab0a67887d6dc4
kondeti shivaji <shivajikondeti555@gmail.com>
2026-02-14 20:29:11 +0530

    next.js updated to 15.0.7 from 15.0.1 version

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  trailingSlash: false,
-  images: {
-    unoptimized: true, // Required for static export with images
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
